2becbe0b6ccfd604633a4d0a6c784efb37d4694c
MelnikkBogdan <115956392+MelnikkBogdan@users.noreply.github.com>
2023-03-10 12:57:43 +0200

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,36 @@
+using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double x = 1, y = 0, z = 0, a = 0;
+            while (x != 0)
+            {
+                x = double.Parse(Console.ReadLine());
+                y = double.Parse(Console.ReadLine());
+                z = double.Parse(Console.ReadLine());
+                a = Math.Atan(y + z) * Math.Pow(Math.Pow(y, 2) + 4, 2);
+                if (x < -1 && x > -10)
+                { Console.WriteLine($"\nменш за -1"); }
+                if (x < -10)
+                {
+                    Console.WriteLine($"\nменш за -10");
+                }
+                if (x > 10)
+                {
+                    Console.WriteLine($"\nбільш за 10");
+                }
+                if (x > 1 && x < 10)
+                {
+                    Console.WriteLine($"\nбільш за 1");
+                }
+                Console.WriteLine(a);
+
+
+                string rez = (a < 0) ? "negative" : "positive";
+                Console.WriteLine($"\nRezult is {rez}");
+            }
+        }
+    }
+
